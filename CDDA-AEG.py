efbc2b452a0a05c22e6c8f27890a7ae08bea7215
Sayurime <mactepxumuk@gmail.com>
2022-01-12 00:02:58 +0300

    "Fits" encumbrance values
    Improved readability.
    Output formatting for GitHub markdown.

--- CDDA-AEG.py
+++ CDDA-AEG.py
@@ -66,7 +66,7 @@ for root, dirs, files in os.walk(armor_data_path):
                                             average_coverage.append(body_part["coverage"] * coverage_multiplayer)
                                             average_coverage = round(sum(average_coverage) / len(average_coverage))
                                             # print("average coverage: " + str(average_coverage))
-                                            armor_object_dict["average_coverage"] = str(average_coverage)
+                                            armor_object_dict["Coverage"] = str(average_coverage)
                                             average_coverage = []
                         else:
                             average_coverage.append(body_part["coverage"])
@@ -76,8 +76,8 @@ for root, dirs, files in os.walk(armor_data_path):
                             average_coverage = []
                     # else:
                     #     average_coverage.append(0)
-                    #     armor_object_dict["average_coverage"] = str(average_coverage)
-                    #     average_coverage = []                       
+                    #     armor_object_dict["Coverage"] = str(average_coverage)
+                    #     average_coverage = []
 
                 for body_part in armor_info:
                     if "encumbrance" in body_part:
@@ -90,7 +90,11 @@ for root, dirs, files in os.walk(armor_data_path):
                 if average_encumbrance != []:
                     average_encumbrance = round(sum(average_encumbrance) / len(average_encumbrance))
                     # print("average encumbrance: " + str(average_encumbrance))
-                    armor_object_dict["average_encumbrance"] = str(round(average_encumbrance))
+                    armor_object_dict["Encumbrance"] = str(average_encumbrance)
+                    if "flags" in armor_object:
+                        for flag in armor_object["flags"]:
+                            if flag == "VARSIZE":
+                                armor_object_dict.update({"Encumbrance": str(round(average_encumbrance / 2))})
 
             if "material_thickness" in armor_object:
                 material_thickness = armor_object["material_thickness"]
@@ -112,26 +116,26 @@ for root, dirs, files in os.walk(armor_data_path):
                     if average_resist != []:
                         average_resist = (sum(average_resist) / len(average_resist)) * material_thickness
                         # print("average resist: " + str(average_resist))
-                        armor_object_dict["average_resist"] = str(round(average_resist, 1))
-            if "average_coverage" in armor_object_dict and "average_encumbrance" in armor_object_dict and "average_resist" in armor_object_dict:
-                armor_efficiency = (int(armor_object_dict["average_coverage"]) / 10) * float(armor_object_dict["average_resist"])
-                if "average_encumbrance" in armor_object_dict and armor_object_dict["average_encumbrance"] != "0":
-                    armor_efficiency = (armor_efficiency / int(armor_object_dict["average_encumbrance"]))
+                        armor_object_dict["Resist"] = str(round(average_resist, 1))
+            if "Coverage" in armor_object_dict and "Encumbrance" in armor_object_dict and "Resist" in armor_object_dict:
+                armor_efficiency = (int(armor_object_dict["Coverage"]) / 10) * float(armor_object_dict["Resist"])
+                if "Encumbrance" in armor_object_dict and armor_object_dict["Encumbrance"] != "0":
+                    armor_efficiency = (armor_efficiency / int(armor_object_dict["Encumbrance"]))
                 # print("armor efficiency: " + str(round(armor_efficiency, 1)))
-                armor_object_dict["armor_efficiency"] = str(round(armor_efficiency, 1))
+                armor_object_dict["Efficiency"] = str(round(armor_efficiency, 1))
                 if "flags" in armor_object:
                     for flag in armor_object["flags"]:
                         if flag == "OUTER":
-                            armor_object_dict["layer"] = str("OUTER")
+                            armor_object_dict["Layer"] = "Outer"
                         if flag == "SKINTIGHT":
-                            armor_object_dict["layer"] = str("SKINTIGHT")
+                            armor_object_dict["Layer"] = "Skintight"
                         if flag == "BELTED":
-                            armor_object_dict["layer"] = str("BELTED")
+                            armor_object_dict["Layer"] = "Strapped"
                         if flag == "WAIST":
-                            armor_object_dict["layer"] = str("WAIST")
-                if "layer" not in armor_object_dict:
-                    armor_object_dict["layer"] = str("MIDDLE")
-                if armor_object_dict["armor_efficiency"] != "0.0":
+                            armor_object_dict["Layer"] = "Waist"
+                if "Layer" not in armor_object_dict:
+                    armor_object_dict["Layer"] = "Normal"
+                if armor_object_dict["Efficiency"] != "0.0":
                     for body_part in armor_info:
                         if "covers" in body_part:
                             for covers in body_part["covers"]:
@@ -165,10 +169,16 @@ markdown_list.append(armor_efficiency_list_hands)
 markdown_list.append(armor_efficiency_list_legs)
 markdown_list.append(armor_efficiency_list_feet)
 
-original_stdout = sys.stdout # Save a reference to the original standard output
-with open('markdow-output.txt', 'w+') as f:
+original_stdout = sys.stdout  # Save a reference to the original standard output
+with open('markdow_output.txt', 'w+') as f:
     for lists in markdown_list:
-        sys.stdout = f # Change the standard output to the file we created.
-        markdown_compiled = Tomark.table(lists)
-        print(markdown_compiled)
-        sys.stdout = original_stdout # Reset the standard output to its original value
+        sys.stdout = f  # Change the standard output to the file we created.
+        markdow_output = Tomark.table(lists)
+        print("##")
+        print("<details>")
+        print("  <summary>Spoiler</summary>")
+        print()
+        print(markdow_output)
+        print("</details>")
+        print()
+        sys.stdout = original_stdout  # Reset the standard output to its original value
