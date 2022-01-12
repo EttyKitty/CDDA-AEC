53eeed1db03bd11899cd06676eb03f865292ec32
Sayurime <mactepxumuk@gmail.com>
2022-01-12 18:45:19 +0300

    Some refactoring

--- CDDA-AEG.py
+++ CDDA-AEG.py
@@ -32,91 +32,96 @@ for root, dirs, files in os.walk(armor_data_path):
         for armor_object in armor_json:
             if "copy-from" in armor_object:
                 continue
+            if "material" in armor_object:
+                for material in armor_object["material"]:
+                    if "type" in material:
+                        continue
+            if "name" not in armor_object:
+                continue
+            if "armor" not in armor_object:
+                continue
+            if "material_thickness" not in armor_object:
+                continue
+            armor_name = armor_object["name"]
+            armor = armor_object["armor"]
+            material_thickness = armor_object["material_thickness"]
             average_coverage = []
             average_coverage_part = []
             average_encumbrance = []
             average_resist = []
             armor_object_dict = {}
-            if "name" in armor_object:
-                armor_name = armor_object["name"]
-                if isinstance(armor_name, str):
-                    # print()
-                    # print("name: " + armor_name)
-                    armor_object_dict["Name"] = str(armor_name)
-                elif "str" in armor_name:
-                    # print()
-                    # print("name: " + armor_name["str"])
-                    armor_object_dict["Name"] = armor_name["str"]
-                elif "str_sp" in armor_name:
-                    # print()
-                    # print("name: " + armor_name["str_sp"])
-                    armor_object_dict["Name"] = armor_name["str_sp"]
-            if "armor" in armor_object:
-                armor_info = armor_object["armor"]
 
-                for body_part in armor_info:
-                    if "coverage" in body_part and "covers" in body_part:
-                        for exact_part in body_part["covers"]:
-                            # print("average coverage: " + str(average_coverage))
-                            if "specifically_covers" in body_part:
-                                for specificall_part in body_part["specifically_covers"]:
-                                    if pattern.search(specificall_part):
-                                        continue
-                                    for limb_object in limbs_json:
-                                        if "max_coverage" in limb_object:
-                                            if limb_object["id"] == specificall_part:
-                                                coverage_multiplayer = (limb_object["max_coverage"] / 100)
-                                                # print(body_part["coverage"])
-                                                # print(coverage_multiplayer)
-                                                average_coverage_part.append(body_part["coverage"] * coverage_multiplayer)
-                                                # print("average coverage: " + str(average_coverage))
-                            else:
-                                average_coverage_part.append(body_part["coverage"])
-                            average_coverage.append(sum(average_coverage_part))
-                            average_coverage_part = []
-                if average_coverage != []:
-                    average_coverage = round(sum(average_coverage) / len(average_coverage))
-                    armor_object_dict["Coverage"] = str(average_coverage)
-                    average_coverage = []
+            if isinstance(armor_object["name"], str):
+                # print()
+                # print("name: " + armor_name)
+                armor_object_dict["Name"] = str(armor_name)
+            elif "str" in armor_object["name"]:
+                # print()
+                # print("name: " + armor_name["str"])
+                armor_object_dict["Name"] = armor_name["str"]
+            elif "str_sp" in armor_object["name"]:
+                # print()
+                # print("name: " + armor_name["str_sp"])
+                armor_object_dict["Name"] = armor_name["str_sp"]
 
-                for body_part in armor_info:
-                    if "encumbrance" in body_part:
-                        if isinstance(body_part["encumbrance"], int):
-                            average_encumbrance.append(body_part["encumbrance"])
+            for body_part in armor:
+                if "coverage" in body_part and "covers" in body_part:
+                    for exact_part in body_part["covers"]:
+                        # print("average coverage: " + str(average_coverage))
+                        if "specifically_covers" in body_part:
+                            for specificall_part in body_part["specifically_covers"]:
+                                if pattern.search(specificall_part):
+                                    continue
+                                for limb_object in limbs_json:
+                                    if "max_coverage" in limb_object:
+                                        if limb_object["id"] == specificall_part:
+                                            coverage_multiplayer = (limb_object["max_coverage"] / 100)
+                                            # print(body_part["coverage"])
+                                            # print(coverage_multiplayer)
+                                            average_coverage_part.append(body_part["coverage"] * coverage_multiplayer)
+                                            # print("average coverage: " + str(average_coverage))
                         else:
-                            average_encumbrance.append(body_part["encumbrance"][0])
-                    else:
-                        average_encumbrance.append(0)
-                if average_encumbrance != []:
-                    average_encumbrance = round(sum(average_encumbrance) / len(average_encumbrance))
-                    # print("average encumbrance: " + str(average_encumbrance))
-                    armor_object_dict["Encumbrance"] = str(average_encumbrance)
-                    if "flags" in armor_object:
-                        for flag in armor_object["flags"]:
-                            if flag == "VARSIZE":
-                                armor_object_dict.update({"Encumbrance": str(round(average_encumbrance / 2))})
+                            average_coverage_part.append(body_part["coverage"])
+                        average_coverage.append(sum(average_coverage_part))
+                        average_coverage_part = []
+            if average_coverage != []:
+                average_coverage = round(sum(average_coverage) / len(average_coverage))
+                armor_object_dict["Coverage"] = str(average_coverage)
+                average_coverage = []
 
-            if "material_thickness" in armor_object:
-                material_thickness = armor_object["material_thickness"]
-                # print("material thickness: " + str(material_thickness))
-                if "material" in armor_object:
-                    i = 0
-                    for material in armor_object["material"]:
-                        for material_object in material_json:
-                            if "id" in material_object:
-                                if material_object["id"] == material:
-                                    i += 1
-                                    # print("material " + str(i) + ":" + str(material))
-                                    if "bash_resist" in material_object:
-                                        # print("bash resist: " + str(material_object["bash_resist"]))
-                                        average_resist.append(material_object["bash_resist"])
-                                    if "cut_resist" in material_object:
-                                        # print("cut resist: " + str(material_object["cut_resist"]))
-                                        average_resist.append(material_object["cut_resist"])
-                    if average_resist != []:
-                        average_resist = (sum(average_resist) / len(average_resist)) * material_thickness
-                        # print("average resist: " + str(average_resist))
-                        armor_object_dict["Resist"] = str(round(average_resist, 1))
+            for body_part in armor:
+                if "encumbrance" in body_part:
+                    if isinstance(body_part["encumbrance"], int):
+                        average_encumbrance.append(body_part["encumbrance"])
+                    else:
+                        average_encumbrance.append(body_part["encumbrance"][0])
+                else:
+                    average_encumbrance.append(0)
+            if average_encumbrance != []:
+                average_encumbrance = round(sum(average_encumbrance) / len(average_encumbrance))
+                # print("average encumbrance: " + str(average_encumbrance))
+                armor_object_dict["Encumbrance"] = str(average_encumbrance)
+                if "flags" in armor_object:
+                    for flag in armor_object["flags"]:
+                        if flag == "VARSIZE":
+                            armor_object_dict.update({"Encumbrance": str(round(average_encumbrance / 2))})
+
+            # print("material thickness: " + str(material_thickness))
+            for material in armor_object["material"]:
+                for material_object in material_json:
+                    if "id" in material_object:
+                        if material_object["id"] == material:
+                            # print("material " + str(i) + ":" + str(material))
+                            if "bash_resist" in material_object:
+                                # print("bash resist: " + str(material_object["bash_resist"]))
+                                average_resist.append(material_object["bash_resist"])
+                            if "cut_resist" in material_object:
+                                # print("cut resist: " + str(material_object["cut_resist"]))
+                                average_resist.append(material_object["cut_resist"])
+            if average_resist != []:
+                average_resist = (sum(average_resist) / len(average_resist)) * material_thickness
+                # print("average resist: " + str(average_resist))
+                armor_object_dict["Resist"] = str(round(average_resist, 1))
 
             if "Coverage" in armor_object_dict and "Encumbrance" in armor_object_dict and "Resist" in armor_object_dict:
                 armor_efficiency = (int(armor_object_dict["Coverage"]) / 10) * float(armor_object_dict["Resist"])
@@ -137,7 +142,7 @@ for root, dirs, files in os.walk(armor_data_path):
                 if "Layer" not in armor_object_dict:
                     armor_object_dict["Layer"] = "Normal"
                 if armor_object_dict["Efficiency"] != "0.0":
-                    for body_part in armor_info:
+                    for body_part in armor:
                         if "covers" in body_part:
                             for covers in body_part["covers"]:
                                 if covers == "torso":
@@ -171,8 +176,8 @@ markdown_list.append(armor_efficiency_list_legs)
 markdown_list.append(armor_efficiency_list_feet)
 
 names = ["Torso", "Head", "Eyes", "Mouth", "Arms", "Hands", "Legs", "Feet"]
-original_stdout = sys.stdout  # Save a reference to the original standard output
 i = 0
+original_stdout = sys.stdout  # Save a reference to the original standard output
 with open('markdow_output.txt', 'w+') as f:
     for lists in markdown_list:
         sys.stdout = f  # Change the standard output to the file we created.
